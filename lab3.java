b7fa59021f07c56cee7bc4013c2ccafd0b223174
ltfranks <114035259+ltfranks@users.noreply.github.com>
2023-02-06 22:23:14 -0800

    Update lab3.java

--- lab3.java
+++ lab3.java
@@ -5,15 +5,16 @@ import java.util.List;
 
 public class lab3 {
 
-    HashMap<String, Integer> Registers = new HashMap<>();
-    HashMap<String, Integer> LabelLocation = new HashMap<>();
-    ArrayList<List<String>> cleanedFile = new ArrayList<>();
-    int LineCount = 0;
+    public static HashMap<String, Integer> registers = new HashMap<>();
+    public static HashMap<String, Integer> LabelLocation = new HashMap<>();
+    public static ArrayList<List<String>> cleanedFile = new ArrayList<>();
+    public static int LineCount = 0;
 
-    int[] data = new int[8192];
+    public static int[] data = new int[8192];
     public static void main(String[] args) throws IOException {
         new Registers();
         if (args.length == 1) {
+            new ScanAssembly(args[0]);
             new InteractiveMode();
         }
         else {new ScanAssembly(args[0]);

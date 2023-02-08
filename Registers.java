7367fdccf2ce3058b41a7da4aa9bf0c85faa2a6c
ltfranks <114035259+ltfranks@users.noreply.github.com>
2023-02-07 16:22:06 -0800

    Update Registers.java

--- Registers.java
+++ Registers.java
@@ -4,40 +4,40 @@ public class Registers extends lab3 {
 
     public Registers(){
 
-        Registers.put("pc", 0);
-        Registers.put("$0", 0);
+        registers.put("pc", 0);
+        registers.put("$0", 0);
 
-        Registers.put("$v0",0);
-        Registers.put("$v1",0);
+        registers.put("$v0",0);
+        registers.put("$v1",0);
 
-        Registers.put("$a0",0);
-        Registers.put("$a1",0);
-        Registers.put("$a2",0);
-        Registers.put("$a3",0);
+        registers.put("$a0",0);
+        registers.put("$a1",0);
+        registers.put("$a2",0);
+        registers.put("$a3",0);
 
-        Registers.put("$t0",0);
-        Registers.put("$t1",0);
-        Registers.put("$t2",0);
-        Registers.put("$t3",0);
-        Registers.put("$t4",0);
-        Registers.put("$t5",0);
-        Registers.put("$t6",0);
-        Registers.put("$t7",0);
+        registers.put("$t0",0);
+        registers.put("$t1",0);
+        registers.put("$t2",0);
+        registers.put("$t3",0);
+        registers.put("$t4",0);
+        registers.put("$t5",0);
+        registers.put("$t6",0);
+        registers.put("$t7",0);
 
-        Registers.put("$s0",0);
-        Registers.put("$s1",0);
-        Registers.put("$s2",0);
-        Registers.put("$s3",0);
-        Registers.put("$s4",0);
-        Registers.put("$s5",0);
-        Registers.put("$s6",0);
-        Registers.put("$s7",0);
+        registers.put("$s0",0);
+        registers.put("$s1",0);
+        registers.put("$s2",0);
+        registers.put("$s3",0);
+        registers.put("$s4",0);
+        registers.put("$s5",0);
+        registers.put("$s6",0);
+        registers.put("$s7",0);
 
-        Registers.put("$t8",0);
-        Registers.put("$t9",0);
+        registers.put("$t8",0);
+        registers.put("$t9",0);
 
-        Registers.put("$sp",0);
-        Registers.put("$ra",0);
+        registers.put("$sp",0);
+        registers.put("$ra",0);
     }
 
 }

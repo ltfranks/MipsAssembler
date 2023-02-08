7b2f8411d52fdf34be5c3b5505df311abf4ac61e
ltfranks <114035259+ltfranks@users.noreply.github.com>
2023-02-08 00:31:22 -0800

    Update lab3.java

--- lab3.java
+++ lab3.java
@@ -9,15 +9,19 @@ public class lab3 {
     public static HashMap<String, Integer> LabelLocation = new HashMap<>();
     public static ArrayList<List<String>> cleanedFile = new ArrayList<>();
     public static int LineCount = 0;
+    public static int LabelTracker = 0;
 
     public static int[] data = new int[8192];
     public static void main(String[] args) throws IOException {
         new Registers();
+
         if (args.length == 1) {
+            new FirstScan(args[0]);
             new ScanAssembly(args[0]);
             new InteractiveMode();
         }
-        else {new ScanAssembly(args[0]);
+        else {
+            new ScanAssembly(args[0]);
             new InteractiveMode(args[1]);
         }
     }
